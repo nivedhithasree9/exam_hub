3f73d81adc2eee834257c81ad51c31dd681187d3
Niveditha Lakshmi <nivedithasree9@gmail.com>
2026-06-09 15:47:14 +0530

    Add remaining project updates

--- server/middleware/errorHandler.js
+++
@@ -1,6 +0,0 @@
-function errorHandler(err, req, res, next) {
-  const status = err.status || 500;
-  res.status(status).json({ message: err.message || 'Server Error' });
-}
-
-module.exports = { errorHandler };
